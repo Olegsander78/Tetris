f692f34b080e7f15624a4d4a131dd1ed5e4ee4e7
Oleg Kotov <onkotov@gmail.com>
2021-12-13 23:59:00 +0500

    Сделал генератор фигур

--- Tetris/Program.cs
+++ Tetris/Program.cs
@@ -11,30 +11,51 @@ namespace Tetris
             Console.SetWindowSize(40, 30);
             Console.SetBufferSize(40, 30);
 
-                       
-            Figure s = new Stick(20, 5, '#');
+            FigureGenerator generator = new FigureGenerator(20, 0, '#');
+            Figure s=null;
+
+
+            while (true)
+            {
+                FigureFall(s,generator);
+                s.Draw();
+            }
+
+            static void FigureFall(Figure fig, FigureGenerator figureGenerator)
+            {
+                fig = figureGenerator.GetNewFigure();
+                fig.Draw();
+
+                for (int i = 0; i < 15; i++)
+                {
+                    Thread.Sleep(500);
+                    fig.Hide();
+                    fig.Move(Direction.DOWN);
+                    fig.Draw();
+                }
+            }
+
+            //s.Draw();
+            //Thread.Sleep(500);
             
-            s.Draw();
-            Thread.Sleep(500);
-            
-            s.Hide();
-            s.Rotate();
-            s.Draw();
+            //s.Hide();
+            //s.Rotate();
+            //s.Draw();
 
-            Thread.Sleep(500);
-            s.Hide();
-            s.Move(Direction.DOWN);
-            s.Draw();
+            //Thread.Sleep(500);
+            //s.Hide();
+            //s.Move(Direction.DOWN);
+            //s.Draw();
 
-            Thread.Sleep(500);
-            s.Hide();
-            s.Move(Direction.RIGTH);
-            s.Draw();
+            //Thread.Sleep(500);
+            //s.Hide();
+            //s.Move(Direction.RIGTH);
+            //s.Draw();
 
-            Thread.Sleep(500);
-            s.Hide();
-            s.Rotate();
-            s.Draw();
+            //Thread.Sleep(500);
+            //s.Hide();
+            //s.Rotate();
+            //s.Draw();
 
             //Figure[] f = new Figure[2];
             //f[0]= new Square(2, 5, '*');
